1f040b4e8402b243e072dd3fae302aca92b76529
Yan Ding <dingyd@mcmaster.ca>
2024-11-07 20:46:25 +0000

    Your commit message here

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 import server from './server'
 
 await server(3000)
+//import server from './server';
+
+//async function startServer() {
+//  await server(3000);
+//}
+
+//startServer();  // Call the async function to start the server
